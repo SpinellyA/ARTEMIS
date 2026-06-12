eaacb31462aba1b0487b9f4e5511b5538672294f
bob <alexsamcabildo7@gmail.com>
2026-06-13 01:52:19 +0800

    Add my password hasher service (cus .net's suck)

---
+++ ProjectARTEMIS/ProjectARTEMIS/Application/PasswordHasher/BcryptPasswordHasher.cs
@@ -0,0 +1,5 @@
+﻿public class BcryptPasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string plainText) => BCrypt.Net.BCrypt.HashPassword(plainText);
+    public bool VerifyPassword(string plainText, string hash) => BCrypt.Net.BCrypt.Verify(plainText, hash);
+}
---
+++ ProjectARTEMIS/ProjectARTEMIS/Application/PasswordHasher/IPasswordHasher.cs
@@ -0,0 +1,5 @@
+﻿public interface IPasswordHasher
+{
+    string HashPassword(string plainText);
+    bool VerifyPassword(string plainText, string hash);
+}
