9df81bf1f982eff0ee78c8b5c65d92a7e64a0483
bob <alexsamcabildo7@gmail.com>
2026-06-13 17:18:37 +0800

    Implenent UserService and its AuthController

---
+++ ProjectARTEMIS/ProjectARTEMIS/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[AllowAnonymous]
+[Route("/api/v1/auth/")]
+public class AuthController : ControllerBase
+{
+    private readonly UserService _user;
+
+    public AuthController(UserService user)
+    {
+        _user = user;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest req)
+    {
+        var token = _user.LoginAsync(req);
+
+        return Ok(token);
+    }
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest req)
+    {
+        var token = _user.RegisterAsync(req);
+        return Ok(token);
+    }
+}
